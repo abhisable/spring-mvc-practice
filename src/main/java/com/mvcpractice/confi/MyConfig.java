adb31cf9f5366f1da727cb5c162c833257ee9f46
Abhishek Sable <abhisheksable@Abhisheks-MacBook-Air.local>
2024-10-20 16:11:38 +0530

    implemented internalResourceViewResolver to render pages effectively

--- src/main/java/com/mvcpractice/confi/MyConfig.java
+++ src/main/java/com/mvcpractice/confi/MyConfig.java
@@ -1,12 +1,17 @@
 package com.mvcpractice.confi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "com.mvcpractice.*")
 public class MyConfig {
 	
-	
+	@Bean
+	public InternalResourceViewResolver getViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}
 
 }
--- src/main/java/com/mvcpractice/controllers/MyController.java
+++ src/main/java/com/mvcpractice/controllers/MyController.java
@@ -2,15 +2,13 @@ package com.mvcpractice.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
 
 	@GetMapping("/home")
-	@ResponseBody
 	public String homePageRequest() {
-		return "displaying home page content";
+		return "home-page";
 		
 	}
 }
